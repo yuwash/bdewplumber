a69c479d4f3c144c047fdf9e2489ae9e6729dcb3
hoi <hoi@yuwash.eu>
2025-12-28 21:37:37 +0000

    chore: add Firebase Studio file

---
+++ .idx/dev.nix
@@ -0,0 +1,27 @@
+{pkgs}: {
+  channel = "unstable";
+  packages = [
+    pkgs.nodejs
+  ];
+  idx.extensions = [
+    "svelte.svelte-vscode"
+    "vue.volar"
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
